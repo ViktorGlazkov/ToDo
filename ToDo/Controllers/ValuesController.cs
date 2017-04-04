a6cc64e0d7ad616218db4196b7b5157596096c33
Viktor Glazkov <viktorg.dev@gmail.com>
2017-04-05 00:31:44 +0300

    Add correct route

--- ToDo/Controllers/ValuesController.cs
+++ ToDo/Controllers/ValuesController.cs
@@ -9,38 +9,19 @@ using ToDo.Models;
 namespace ToDo.Controllers
 {
     [Authorize]
+    [RoutePrefix("api")]
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<Value> Get()
+        [Route("tasks"), HttpGet]
+        public IEnumerable<Task> Get()
         {
-            var values = new List<Value>();
-            values.Add(new Value("value1"));
-            values.Add(new Value("value2"));
-            values.Add(new Value("value3"));
+            var values = new List<Task>();
+            values.Add(new Task("value1"));
+            values.Add(new Task("value2"));
+            values.Add(new Task("value3"));
 
             return values;
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        }    
     }
 }

83fd8496ccf80de853e720d3cc939cdb6f29a989
Viktor Glazkov <viktorg.dev@gmail.com>
2017-04-04 09:33:16 +0300

    Add base config

---
+++ ToDo/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Value> Get()
+        {
+            var values = new List<Value>();
+            values.Add(new Value("value1"));
+            values.Add(new Value("value2"));
+            values.Add(new Value("value3"));
+
+            return values;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ToDo/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Value
+    {
+        public string Name { get; set; }
+
+        public Value(string name)
+        {
+            Name = name;
+        }
+    }
+}

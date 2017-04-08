8a0fc578db4f4694ea353e3eabdeb964d668c9a5
Viktor Glazkov <viktorg.dev@gmail.com>
2017-04-08 22:38:51 +0300

    Add navbar component

---
+++ NgToDo/src/app/component/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+
+export class NavbarComponent {}
